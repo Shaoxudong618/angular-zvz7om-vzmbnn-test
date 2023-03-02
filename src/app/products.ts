764c385afde31ae9f19a4734f767e44e30577dad
Shaoxudong618 <126746944+Shaoxudong618@users.noreply.github.com>
2023-03-02 13:23:57 +0800

    3 files modified, 4 added

--- src/app/products.ts
+++ src/app/products.ts
@@ -4,28 +4,27 @@ export interface Product {
   price: number;
   description: string;
 }
-
+//常量
 export const products = [
   {
     id: 1,
     name: 'Phone XL',
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: 'A large phone with one of the best screens',
   },
   {
     id: 2,
     name: 'Phone Mini',
     price: 699,
-    description: 'A great phone with one of the best cameras'
+    description: 'A great phone with one of the best cameras',
   },
   {
     id: 3,
     name: 'Phone Standard',
     price: 299,
-    description: ''
-  }
+    description: '',
+  },
 ];
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
